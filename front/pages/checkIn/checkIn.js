25496cb0e1a8ccfd028d5372df7e4fbd74f38ade
lyq <524123601@qq.com>
2019-12-22 00:02:50 +0800

    签到页面布局完成

--- front/pages/checkIn/checkIn.js
+++ front/pages/checkIn/checkIn.js
@@ -1,28 +1,42 @@
 // pages/checkIn/checkIn.js
-import {getUserToken} from '../../utils/userTokenManager'
+import { getUserToken } from '../../utils/userTokenManager'
+import Notify from '../../dist/vant-weapp/notify/notify';
+import { AaHostPost } from '../../utils/httpManager'
+
+const checkStatus = {
+	notNeed: 0,  //没课
+	notYet: 1,   //还没打卡
+	checked: 2,  //打卡了
+};
 
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		//state
+		courseNow: "形式与政策",
+		isCheckin: -1,
+		//display
+		buttonDisable: false,
+		buttonLoading: false,
+		buttonText: "签 到"
+	},
+	//生命周期函数--监听页面加载
+	onLoad: function (options) {
+		const { userId } = getUserToken();
+		if (!userId && userId !== 0) {
+			//未登录
+			wx.reLaunch({
+				url: "../login/login",
+			});
+		} else {
+			//已登录
+			this.fetchCourseInfo();
+		}
+	},
+	//获取当前时间，本同学的课
+	fetchCourseInfo: function () {
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    let userId = getUserToken();
-    if(!userId){
-      //未登录
-      wx.reLaunch({
-        url: "../login/login",  
-      });
-    }else{
-      //已登录
-      console.log(userId)
-    }
-  }
+	}
 })

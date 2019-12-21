533a9eb8ed65d3931fa45ebb6ce5970cf7f169e4
lyq <524123601@qq.com>
2019-12-22 02:28:56 +0800

    fetchCourseInfo完成四分之三

--- server/src/main/java/database/exp/aa/service/aaServiceImplememt/CheckInService.java
+++ server/src/main/java/database/exp/aa/service/aaServiceImplememt/CheckInService.java
@@ -2,6 +2,7 @@ package database.exp.aa.service.aaServiceImplememt;
 
 import com.alibaba.fastjson.JSONObject;
 import database.exp.aa.mapper.CourseMapper;
+import database.exp.aa.pojo.Course;
 import database.exp.aa.pojo.StudentCourseMap;
 import database.exp.aa.service.aaServiceInterface.CheckInServiceInterface;
 import database.exp.aa.util.AaResponse;
@@ -17,12 +18,29 @@ public class CheckInService implements CheckInServiceInterface {
 
     @Override
     public AaResponse<Map<String, Object>> getCourseInfoByUser(JSONObject parameters) {
+        //时间处理
         Calendar now = Calendar.getInstance();
         now.setTime(new Date((long)parameters.get("nowTimestamp")));
+        int dayIndex = now.get(Calendar.DAY_OF_WEEK);
+        int courseIndex = (now.get(Calendar.HOUR_OF_DAY)<12)?1:2;
+
+        //找有没有课
         List<StudentCourseMap> courses = courseMapper.getAllCoursesByStudentId((int)parameters.get("studentId"));
         Iterator<StudentCourseMap> it = courses.iterator();
+        Course c = null;
         while (it.hasNext()){
-            System.out.println(it.next().getCourse());
+            c = it.next().getCourse();
+            if(
+                c.getDayIndex() == dayIndex &&
+                c.getCourseIndex() == courseIndex
+            ){
+                break;
+            }
+        }
+        if(c!=null){
+            //有课
+        }else {
+            //没课
         }
         return null;
     }
